4676191113cacf72fa5b6da14ba5263358df8709
EvanJ0hnson <ivangerasimow@gmail.com>
2016-03-14 16:44:39 +0400

    Implement syntax highlight

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,7 @@
 'use strict';
 
 import {Cart} from './modules/cart';
+import hljs from 'highlight.js';
 
 /**
  * Initialization
@@ -11,6 +12,7 @@ document.addEventListener('DOMContentLoaded', () => {
     case '/':
       const cartInstance = new Cart('cart-widjet');
       cartInstance.init();
+      hljs.initHighlighting();
       break;
     default:
       console.log('Error: missing state');
