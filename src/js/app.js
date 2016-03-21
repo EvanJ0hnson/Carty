3f66cc463d7169c27f4939daa5597586825f8cb1
EvanJ0hnson <ivangerasimow@gmail.com>
2016-03-21 15:45:47 +0400

    Move Google Analytics to bundle

--- src/js/app.js
+++ src/js/app.js
@@ -9,6 +9,14 @@ import {toggleNavigationBar} from './modules/toggleNavigation';
  */
 document.addEventListener('DOMContentLoaded', () => {
   const currentState = window.location.pathname;
+
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+  ga('create', 'UA-59739314-4', 'auto');
+  ga('send', 'pageview');
+  
   switch (currentState) {
     case '/':
       const cartInstance = new Cart('cart-widget');
