c5993229c4afd4b8aaaa2d24a1d58cdf6082493b
EvanJ0hnson <ivangerasimow@gmail.com>
2016-04-01 16:17:02 +0400

    Create initApp() function

--- src/js/app.js
+++ src/js/app.js
@@ -5,20 +5,25 @@ import hljs from 'highlight.js';
 import {toggleNavigationBar} from './modules/toggleNavigation';
 
 /**
- * Initialization
+ * Application entry point
  */
-document.addEventListener('DOMContentLoaded', () => {
+const initApp = () => {
   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
   })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
   ga('create', 'UA-59739314-4', 'auto');
   ga('send', 'pageview');
-  
+
+  toggleNavigationBar();
+
   const cartInstance = new Cart('cart-widget');
   cartInstance.init();
 
   hljs.initHighlighting();
+};
 
-  toggleNavigationBar();
-});
+/**
+ * Initialization
+ */
+document.addEventListener('DOMContentLoaded', initApp);
